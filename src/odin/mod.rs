9232a5e8c4a4bd9366e3df122546da976abe18cc
paran3xus <paran3xus007@gmail.com>
2026-02-24 23:10:12 +0800

    feat: highlevel odin edit for udon_program and udon_variable_table

--- src/odin/mod.rs
+++ src/odin/mod.rs
@@ -3,8 +3,12 @@ mod io;
 mod model;
 mod parse;
 mod serialize;
+mod udon_program;
+mod udon_variable_table;
 
 pub use model::{
     NodeId, NodeKind, OdinDocument, OdinError, OdinGuid, OdinNode, OdinString, OdinTypeRef,
     PrimitiveValue, Result, StringEncoding,
 };
+pub use udon_program::{HeapDumpItem, SymbolItem, SymbolSection, UdonProgramBinary};
+pub use udon_variable_table::{UdonVariableTableBinary, VariableItem};
